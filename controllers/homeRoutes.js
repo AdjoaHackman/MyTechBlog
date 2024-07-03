c37097c1fe8848dfd8b8d6b1150ea2058d9aaeaf
Adjoa Hackman <adjoahackman93@gmail.com>
2024-07-03 00:39:37 -0400

    adding a bunch of fixes with help from Michael

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -21,14 +21,23 @@ router.get('/post/:id', async (req, res) => {
   try {
     // Get all users, sorted by name
     const postData = await Post.findByPk(req.params.id);
-
+    console.log(req.params);
     // Serialize user data so templates can read it
     const post = postData.get({
       plain:true
     })
-    console.log(post)
+    console.log(post);
     // Pass serialized data into Handlebars.js template
     res.render('post', { post });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+    // Pass serialized data into Handlebars.js template
+    res.render('login');
   } catch (err) {
     res.status(500).json(err);
   }
---
+++ public/css/js/login.js
@@ -0,0 +1,8 @@
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", function (){
+    const username = document.getElementById("username").value;
+    console.log(username);
+})
+
+onsubmit
